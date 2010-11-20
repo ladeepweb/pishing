c2f90089a028d4a933a47460735e7215cd259fe5
Ferdinand E. Silva <ferdie_v143@yahoo.com>
2010-11-20 11:58:09 +0800

    add message and nick extractor

--- irc.php
+++ irc.php
@@ -11,16 +11,16 @@
 
 class PHPIRC {
 
-    private $IrcServer="";
-    private $IrcPort=6667;
+    private $IrcServer = "";
+    private $IrcPort = 6667;
     
 
-    private $IrcNick="";
-    private $IrcRoom="";
+    private $IrcNick = "";
+    private $IrcRoom = "";
 
     private $socket;
-    private $isConnected=false;
-    private $isAuthenticated=false;
+    private $isConnected = false;
+    private $isAuthenticated = false;
 
 
     public function __construct() {
@@ -29,23 +29,23 @@ class PHPIRC {
 
     private function main() {
         //get IRC Server
-        $this->IrcServer=$this->getUserInput("Please Enter Irc Server Address");
+        $this->IrcServer = $this->getUserInput("Please Enter Irc Server Address");
 
         //get Port
         if($this->getUserInput("Do You Want To Change The Irc Port? The Default Port is " . $this->IrcPort . ". Enter y to change") == "y") {
-            $this->IrcPort=(int)$this->getUserInput("Please Enter Port Number");
+            $this->IrcPort = (int)$this->getUserInput("Please Enter Port Number");
         }
 
         //get Nick
-        $this->IrcNick=$this->getUserInput("Please Enter Irc Nick");
+        $this->IrcNick = $this->getUserInput("Please Enter Irc Nick");
         //get Irc Channel
-        $this->IrcRoom=$this->getUserInput("Please Enter Irc Channel");
+        $this->IrcRoom = $this->getUserInput("Please Enter Irc Channel");
 
         $this->connect(); //connect to irc server
     }
 
     private function getUserInput($msg) {
-        $endInput=false;
+        $endInput = false;
 
         while(!$endInput) {
             echo "\n" . $msg . ": ";
@@ -63,12 +63,12 @@ class PHPIRC {
 
     private function connect() {
         
-        $this->socket=socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
+        $this->socket = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
         
         if(@socket_connect($this->socket, $this->IrcServer,$this->IrcPort)) {
             //connected
             //read messages
-            $this->isConnected=true;
+            $this->isConnected = true;
             
             $this->receiveMessages();
         }else{
@@ -88,15 +88,15 @@ class PHPIRC {
 
         while($this->isConnected) {
 
-            $buffer="";
-            $flag=socket_recv($this->socket, $buffer, 1024,0);
+            $buffer = "";
+            $flag = socket_recv($this->socket, $buffer, 1024,0);
 
             if($flag < 0) {
                 //error
             }elseif($flag == 0) {
                 //disconnected
-                $this->isConnected=false;
-                $this->isAuthenticated=false;
+                $this->isConnected = false;
+                $this->isAuthenticated = false;
                 echo "\nClient Disconnected.\n";
             }else{
                 //messages
@@ -125,6 +125,23 @@ class PHPIRC {
                 }elseif(preg_match("/PRIVMSG \#" . $this->IrcRoom . "/i", $buffer)) {
                     //room message
                     //dito ilalagay yung mga commands
+                    
+                    $tmpStr = preg_split("/:/", $buffer);
+					$tmpStr = preg_split("/!/", $buffer);
+					$nickSender = $tmpStr[0]; //nick of the sender
+					
+					$tmpStr = preg_split("/PRIVMSG #" . $this->IrcRoom . " :/i", $buffer);
+					$messageReceived = $tmpStr[1]; //message received
+					
+					//add command handler below
+					
+					
+					//end of command handler
+					
+					$tmpStr = NULL;
+					$nickSender = "";
+					$messageReceived = "";
+                    
                 }
 
             }
@@ -138,6 +155,6 @@ class PHPIRC {
 
 
 //run PHPIRC
-$run=new PHPIRC();
+$run = new PHPIRC();
 
 ?>
